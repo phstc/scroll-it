63b5392ac4220abcb17cfc153401f8225ed49583
Pablo <pablohstc@gmail.com>
2010-12-09 18:23:01 -0200

    Initial commit

---
+++ scroll-it.js
@@ -0,0 +1,37 @@
+javascript:(function(){
+	 /*@author Pablo Cantero - http://pablocantero.com*/
+	if(window.scrollIt === undefined){
+		window.scrollIt = {
+			intervalId: null
+		};
+	}
+	function getScrollMaxSize(){
+		return document.body.clientHeight;
+	};
+	function getScrollCurrentSize(){
+		return document.documentElement.scrollTop;
+	};
+	window.scrollIt.initialSize = getScrollCurrentSize();
+	window.scrollIt.scrolled = getScrollCurrentSize();
+	var scroll = function(){
+		window.scrollIt.scrolled += 10;
+		window.scrollTo(0, window.scrollIt.scrolled);
+		if(window.scrollIt.scrolled >= getScrollMaxSize()){
+			window.scrollIt.scrolled = window.scrollIt.initialSize;
+		} 
+	};
+	var stop = function(){
+		window.clearInterval(window.scrollIt.intervalId);
+		window.scrollIt.intervalId == null;
+		alert('1');
+	}
+	var start = function(){
+		window.scrollIt.intervalId = window.setInterval(scroll, 1000);
+		alert('2');
+	}
+	if(window.scrollIt.intervalId != null){
+		stop();
+	} else {
+		start();
+	}
+})()
