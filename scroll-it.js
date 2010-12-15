c9fc39475641bfe9ac97386a8ce1996226a1e478
Pablo <pablohstc@gmail.com>
2010-12-15 15:06:19 -0200

    readme updates

--- scroll-it.js
+++ scroll-it.js
@@ -14,7 +14,6 @@ javascript:(function(){
 	window.scrollIt.initialSize = getScrollCurrentSize();
 	window.scrollIt.scrolled = getScrollCurrentSize();
 	var scroll = function(){
-		alert('1');
 		window.scrollIt.scrolled += 10;
 		window.scrollTo(0, window.scrollIt.scrolled);
 		if(window.scrollIt.scrolled >= getScrollMaxSize()){
@@ -24,10 +23,13 @@ javascript:(function(){
 	var stop = function(){
 		window.clearInterval(window.scrollIt.intervalId);
 		window.scrollIt.intervalId = null;
-	}
+	};
 	var start = function(){
-		alert('2');
 		window.scrollIt.intervalId = window.setInterval(scroll, 1000);
-	}
-	start();
+	};
+	if(window.scrollIt.intervalId != null){
+		stop();
+	} else {
+		start();
+	};
 })()
